ddaae2004f330ce7172f394c97bf8ac3e5b6a970
Frank Mooradian <EclecticRenaissanceFrank@Gmail com>
2019-02-07 07:46:27 -0500

    Vocoach chart

--- scripts/bandtastic-chart.js
+++ scripts/bandtastic-chart.js
@@ -1,16 +1,16 @@
 ctx = document.getElementById('bandastic-mvp-graph').getContext('2d');
 var chart = new Chart(ctx, {
   // The type of chart we want to create
-  type: 'bar',
+  type: 'doughnut',
 
   // The data for our dataset
   data: {
       labels: ["Purchase New Gear", "Purchase Used Gear", "Sell Gear", "Rent Gear", "Repair Gear", "Take Music Lessons"],
       datasets: [{
-          label: "Choose the most (1) to least (6) important to you. I want to...",
-          backgroundColor: '#1863CD',
+          label: ". I want to...",
+          backgroundColor: ['#1863CD', '#5E35B1', '#2E7D32', '#ccc', '#ccc', '#ccc'],
           borderWidth: 2,
-          borderColor: '#4B9BFF',
+          borderColor: '#ffffff',
           data: [12, 12, 16, 19, 17, 16],
       }]
       
